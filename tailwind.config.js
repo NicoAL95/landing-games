e38367c2d56592526ac587d1642bf2628320d55a
NicoAL95 <nicoabellaia@gmail.com>
2022-06-02 17:14:47 +0700

    Update Picture Ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      padding: {
+        '1/2': '50%',
+        full: '100%',
+      },
+    },
     fontFamily: {
       'mainFont': 'Montserrat',
     }
   },
-  plugins: [],
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    // ...
+  ],
 }
